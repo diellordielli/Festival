e52943b30702edf520cbedae4ba68779e5cccd14
diellordielli <dd@feinheit.ch>
2013-02-13 15:32:52 +0100

    views edited

--- webapp/urls.py
+++ webapp/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    # Sponsor
+    # Home
     url(r'^$', 'webapp.views.home', name='home'),
 
 )
--- webapp/views.py
+++ webapp/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 
 from sponsors.models import Sponsor, Category, SponsorCategoryYear
 from gallery.models import Image
+from festival.models import Band, Year, BandYear
 
 
 def home(request):
     sponsors = Sponsor.objects.all()
     categories = Category.objects.all()
     images = Image.objects.all()
-    return render(request, 'index.html', {'sponsors':sponsors, 
-        'categories':categories, 'images':images})
+    bands = Band.objects.all()
+    years = Year.objects.all() 
+
+    return render(request, 'index.html', {
+        'sponsors':sponsors,
+        'categories':categories,
+        'images':images,
+        'bands':bands,
+        'years':years})
