2d5b87f34282021f2ee45993127d7fb6ec2747eb
diellordielli <dd@feinheit.ch>
2013-03-11 16:26:48 +0100

    add defaulting to sqlite

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,8 +25,7 @@ DATABASES = {}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
+DATABASES = {'default': dj_database_url.config(default='sqlite:///%s' % os.path.join(PROJECT_ROOT, 'backend.db'))}
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
