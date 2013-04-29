2c5a7a032813a0ced41b000d43087eaf1a1b1cef
diellordielli <dd@feinheit.ch>
2013-04-29 13:13:57 +0200

    south

--- webapp/settings.py
+++ webapp/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'webapp',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
