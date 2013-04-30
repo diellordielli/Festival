010aa9b900cc2e8025a3f9d3ccd91b2eadead03f
Simon Schmid <ssc@feinheit.ch>
2013-04-30 19:59:53 +0000

    hack around band detail

--- webapp/static/js/ajax_bands.js
+++ webapp/static/js/ajax_bands.js
@@ -3,18 +3,25 @@ $(document).ready(function(){
         event.preventDefault();
 
         var $this = $(this),
-            url = $(this).attr('href');
+            url = $(this).attr('href'),
+            closestAjax = $this.closest('.bandsingle').nextAll('.ajaxbands').first();
 
         $.get(url, function(data){
-            var closestAjax = $this.closest('.bandsingle').nextAll('.ajaxbands').first();
-           
+            closestAjax.hide().html(data).slideDown(1500, function() {
+                closestAjax.get(0).style.display='none';
+                closestAjax.get(0).offsetHeight;
+                closestAjax.get(0).style.display='block';
+            });
+
             scroll_to_target(closestAjax);
 
-            closestAjax.hide().html(data).slideDown(1500);
-            closestAjax.get(0).offsetHeight
+            // force redraw hack: http://stackoverflow.com/questions/3485365/how-can-i-force-webkit-to-redraw-repaint-to-propagate-style-changes
+            closestAjax.get(0).style.display='none';
+            closestAjax.get(0).offsetHeight;
+            closestAjax.get(0).style.display='block';
 
             $('.bandclose').on('click', function(event){
-                closestAjax.slideUp(300).html("");
+                closestAjax.slideUp(300);
 
                 target = $('#bands');
                 scroll_to_target(target);
@@ -23,9 +30,11 @@ $(document).ready(function(){
         });
 
         function scroll_to_target(target){
-           $('html,body').delay('300').animate({
+            console.log(target, target.offset().top);
+
+           $('body').delay('300').animate({
                  scrollTop: target.offset().top - 170
-            }, 800);
+            }, 1000);
         }
     });
 });
