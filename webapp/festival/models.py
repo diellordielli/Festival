d0e66f54d4d3002c6da13d6934b51d82e21397ab
diellordielli <dd@feinheit.ch>
2013-05-02 13:06:04 +0200

    choicefield name change

--- webapp/festival/models.py
+++ webapp/festival/models.py
@@ -89,7 +89,7 @@ class BandYear(models.Model):
 
     LINK_TYPES = (
         (HAUPTBUEHNE, 'Hauptbühne'),
-        (ZELTBUEHNE, 'Zeltbühne'),
+        (ZELTBUEHNE, 'Baumhaus'),
     )
 
     stage = models.CharField(max_length=20, choices=LINK_TYPES)
