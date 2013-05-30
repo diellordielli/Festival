fb0e7345693680966697b466e6ee191b8f7106c6
diellordielli <dd@feinheit.ch>
2013-05-30 15:52:55 +0200

    fixed hover effect and change sponsorslider by mediaqueries

--- webapp/static/js/sponsorslider.js
+++ webapp/static/js/sponsorslider.js
@@ -1,7 +1,7 @@
 $(window).load(function() {
 
     var sponsorscroller = $('.sponsorks').length * $('.sponsorks').outerWidth(true) * 2;
-
+    
     $('.sponsorinner').width(sponsorscroller);
 
     var clone = $('.sponsorks').clone();
@@ -9,10 +9,31 @@ $(window).load(function() {
     $('.sponsorinner').append(clone);
 
     function animate() {
+
         $('.sponsorinner').css('left', 0);
         $('.sponsorinner').animate({'left': -sponsorscroller / 2}, 25000, 'linear', animate);
 
     }
+
+    var checkResponsiveNav = function() {
+
+        var sponsorscrollerresponsive = $('.sponsorinner').css({'max-width': '660px'});
+
+        if ($(window).width() < 660) {
+            $('.sponsorinner').stop().animate({ left: 0 });
+            $('.sponsorinner').css({'max-width': sponsorscrollerresponsive});
+            $('.sponsorinner').css({'width':'100%'})
+            clone.hide();
+        } else if ($(window).width() > 660) {
+            $('.sponsorinner').css({'max-width': 'none'});
+            $('.sponsorinner').width(sponsorscroller);
+            $('.sponsorinner').animate({'left': -sponsorscroller / 2}, 25000, 'linear', animate);
+            clone.show();
+        }
+    };
+
+    $(window).resize(checkResponsiveNav);
+    checkResponsiveNav();
 
     animate();
 
